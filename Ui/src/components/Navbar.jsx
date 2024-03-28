1111ff008cef4731957e0c32c87f8956a54d34e5
hamza24v <107317017+hamza24v@users.noreply.github.com>
2024-03-28 15:45:27 -0400

    changed drop down color

--- Ui/src/components/Navbar.jsx
+++ Ui/src/components/Navbar.jsx
@@ -6,9 +6,9 @@ import dadir_design from '../assets/dadir_design.png'
 
 const menuItems = ['Home', 'Services', 'Galleries', 'About'];
 const serviceMenu = [
-  {label: 'Indoor Furniture Assembly',path:'/indoor-furniture-assembly'},
-  {label: 'Furniture Delivery', path: '/furniture-delivery'},
-  {label: 'Outdoor Furniture Assembly',path:'/outdoor-furniture-assembly'}
+  { label: 'Indoor Furniture Assembly', path: '/indoor-furniture-assembly' },
+  { label: 'Furniture Delivery', path: '/furniture-delivery' },
+  { label: 'Outdoor Furniture Assembly', path: '/outdoor-furniture-assembly' }
 ]
 
 const Navbar = () => {
@@ -16,10 +16,10 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const scrollToTop = () => {
     window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
+      top: 0,
+      behavior: 'smooth'
     });
-};
+  };
   const toggleMobileMenu = () => {
     setIsMobileMenuVisible(!isMobileMenuVisible);
   };
@@ -34,14 +34,20 @@ const Navbar = () => {
       <div className='max-w-6xl mx-auto px-4'>
         <div className='flex justify-between items-center py-4'>
           <NavLink to="/" onClick={scrollToTop} className='text-3xl font-serif  font-bold text-gray-900 hover:text-blue-100'>
-                <img className='w-20' src={dadir_design} alt='dadir design' />            
+            <img className='w-20' src={dadir_design} alt='dadir design' />
           </NavLink>
           {/* Desktop Menu */}
           <div className={`hidden sm:hidden md:flex lg:flex xl:flex 2xl:flex items-center`}>
             {menuItems.map((menu, idx) => (
               <>
                 {menu === 'Services' ?
-                  <DropDown key={idx} title="Services" items={serviceMenu} toggleDropdown={toggleDropdown} onClose={onClose} open={isOpen}/>
+                  <DropDown
+                    key={`service-${idx}`}
+                    title="Services"
+                    items={serviceMenu}
+                    toggleDropdown={toggleDropdown}
+                    onClose={onClose}
+                    open={isOpen} />
                   :
                   <NavLink
                     key={idx}
@@ -68,7 +74,7 @@ const Navbar = () => {
         <ul className="flex flex-col items-center">
           {menuItems.map((menu, idx) => (
             <li key={idx} className='py-2'>
-             <>
+              <>
                 {menu === 'Services' ?
                   <DropDown title="Services" items={serviceMenu} />
                   :
