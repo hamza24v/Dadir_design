79aa8af72c67497214fca83c2ee469a024667f48
hamza24v <107317017+hamza24v@users.noreply.github.com>
2024-03-28 15:44:37 -0400

    fixed onclickaway warning

--- Ui/src/components/DropDown.jsx
+++ Ui/src/components/DropDown.jsx
@@ -1,5 +1,4 @@
-// DropdownMenu.jsx
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
@@ -8,34 +7,36 @@ import { ClickAwayListener } from '@mui/material';
 const DropDown = ({ title, items, open, onClose, toggleDropdown }) => {
   const close = onClose ?? false;
   return (
-    <ClickAwayListener onClickAway={close}>
-      <div className="relative">
-        <div className='cursor-pointer hover:cursor-pointer hover:text-blue-100 transition duration-300'>
-          <button
-            className="py-2 ml-4 font-semibold"
-            onClick={toggleDropdown}
-          >
-            {title}
-          </button>
-          {!open && <KeyboardArrowDownIcon onClick={toggleDropdown} />}
-          {open && <KeyboardArrowUpIcon onClick={toggleDropdown} />}
-        </div>
-        {open && (
-          <div className="absolute left-0 mt-2 py-2 bg-orange-400 shadow-xl w-auto">
-            {items.map((item, index) => (
-              <NavLink
-                key={index}
-                to={item.path}
-                className="block px-8 py-2 text-md hover:bg-orange-300 whitespace-nowrap"
-                onClick={() => setIsOpen(false)}
-              >
-                {item.label}
-              </NavLink>
-            ))}
+    <>
+      {close && <ClickAwayListener onClickAway={onClose}>
+        <div className="relative">
+          <div className='cursor-pointer hover:cursor-pointer hover:text-blue-100 transition duration-300'>
+            <button
+              className="py-2 ml-4 font-semibold"
+              onClick={toggleDropdown}
+            >
+              {title}
+            </button>
+            {!open && <KeyboardArrowDownIcon onClick={toggleDropdown} />}
+            {open && <KeyboardArrowUpIcon onClick={toggleDropdown} />}
           </div>
-        )}
-      </div>
-    </ClickAwayListener>
+          {open && (
+            <div className="absolute left-0 mt-2 py-2 bg-orange-300 shadow-xl w-auto">
+              {items.map((item, index) => (
+                <NavLink
+                  key={index}
+                  to={item.path}
+                  className="block px-8 py-2 text-md hover:bg-orange-200 whitespace-nowrap"
+                  onClick={() => setIsOpen(false)}
+                >
+                  {item.label}
+                </NavLink>
+              ))}
+            </div>
+          )}
+        </div>
+      </ClickAwayListener>}
+    </>
   );
 };
 
