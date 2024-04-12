9107d6e03b39c1292870782fc7c4c95a88fb285e
hamza24v <107317017+hamza24v@users.noreply.github.com>
2024-04-11 21:29:05 -0400

    set up backend

--- Ui/src/components/ServiceCard.jsx
+++ Ui/src/components/ServiceCard.jsx
@@ -5,11 +5,11 @@ function ServiceCard({ title, description }) {
 
     return (
         <div className="group w-[300px] h-auto p-5 m-2 bg-orange-300 border border-gray-300 shadow-md hover:shadow-xl rounded-lg cursor-pointer transition-all duration-300 ease-in-out transform">
-            <div className={`flex flex-col h-full`}>
+            <div className='flex flex-col h-full'>
                 <div className="flex justify-center">
                     <h3 className="text-xl font-semibold">{title}</h3>
                 </div>
-                <div className=''>
+                <div className='mb-5'>
                     <p className="mt-2">{description}</p>
                 </div>
                 <Button
