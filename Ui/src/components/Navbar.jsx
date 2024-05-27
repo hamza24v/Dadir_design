f80aacf254b06287d65b53e1d177fafad39f281f
hamza24v <107317017+hamza24v@users.noreply.github.com>
2024-05-27 13:28:37 -0400

    fixed navbar issue

--- Ui/src/components/Navbar.jsx
+++ Ui/src/components/Navbar.jsx
@@ -1,80 +1,76 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
-import DropDown from './DropDown';
-import dadir_design from '../assets/dadir_design.png'
+import DropDown from './DropDown'; // Assuming DropDown is correctly implemented
+import dadir_design from '../assets/dadir_design.png';
+import { ClickAwayListener } from '@mui/material';
 
 const menuItems = ['Home', 'Shop', 'Galleries', 'About'];
 
-
 const Navbar = () => {
-  const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);
+  const [open, setOpen] = useState(false);
 
-  const scrollToTop = () => {
+  const handleNavClick = (event) => {
+    setOpen((prev) => !prev);
+    event.stopPropagation(); // Prevents click from immediately triggering ClickAway
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
     });
   };
-  const toggleMobileMenu = () => {
-    setIsMobileMenuVisible(!isMobileMenuVisible);
-  };
 
+  const toggleMenu = () => {
+    setOpen((prev) => !prev);
+  };
 
   return (
     <nav className='bg-blue-200 shadow fixed top-0 w-full z-10'>
       <div className='max-w-6xl mx-auto px-4'>
-        <div className='flex justify-between items-center py-4'>
-          <NavLink to="/" onClick={scrollToTop} className='text-3xl font-serif  font-bold text-gray-900 hover:text-blue-100'>
-            <img className='w-20' src={dadir_design} alt='dadir design' />
-          </NavLink>
-          {/* Desktop Menu */}
-          <div className={`hidden sm:hidden md:flex items-center`}>
-            {menuItems.map((menu, idx) => (
-                <NavLink
-                  key={idx}
-                  onClick={scrollToTop}
-                  to={"/" + (menu === 'Home' ? '' : menu)}
-                  className='py-2 px-4 text-gray-900 font-semibold hover:text-white transition duration-300'
-                >
-                  {menu === 'About' ? menu + ' Us' : menu}
-                </NavLink>
-            ))}
-          </div>
-
-          {/* Mobile Menu Button */}
-          <div className='sm:flex md:hidden'>
-            <MenuIcon onClick={toggleMobileMenu} className='hover:text-white' />
-          </div>
-        </div>
-      </div>
-
-      {/* Mobile Menu */}
-      <div className={`${isMobileMenuVisible ? 'block' : 'hidden'} md:hidden`}>
-        <ul className="flex flex-col items-center">
-          {menuItems.map((menu, idx) => (
-            <li key={idx} className='py-2'>
-              <>
-                {menu === 'Services' ?
-                  <DropDown title="Services" items={serviceMenu} />
-                  :
+        <ClickAwayListener onClickAway={() => setOpen(false)}>
+          <div className='flex flex-col'>
+            <div className='flex justify-between items-center py-4'>
+              <NavLink to="/" onClick={handleNavClick} className='text-3xl font-serif font-bold text-gray-900 hover:text-blue-100'>
+                <img className='w-20' src={dadir_design} alt='dadir design' />
+              </NavLink>
+              {/* Desktop Menu */}
+              <div className={`hidden sm:hidden md:flex items-center`}>
+                {menuItems.map((menu, idx) => (
                   <NavLink
                     key={idx}
-                    onClick={scrollToTop}
                     to={"/" + (menu === 'Home' ? '' : menu)}
-                    className='py-2 px-4 text-gray-900 font-semibold hover:text-blue-100 transition duration-300'
+                    onClick={handleNavClick}
+                    className='py-2 px-4 text-gray-900 font-semibold hover:text-white transition duration-300'
                   >
                     {menu === 'About' ? menu + ' Us' : menu}
                   </NavLink>
-                }
-              </>
-            </li>
-          ))}
-        </ul>
+                ))}
+              </div>
+              {/* Mobile Menu */}
+              <div className='sm:flex md:hidden'>
+                <MenuIcon onClick={toggleMenu} className='hover:text-white' />
+              </div>
+            </div>
+            {open && (
+              <ul className="text-center justify-center md:hidden">
+                {menuItems.map((menu, idx) => (
+                  <li key={idx} className='py-2 w-full'>
+                    <NavLink
+                      to={"/" + (menu === 'Home' ? '' : menu)}
+                      onClick={handleNavClick}
+                      className='py-2 px-4 text-gray-900 font-semibold hover:text-blue-100 transition duration-300'
+                    >
+                      {menu === 'About' ? menu + ' Us' : menu}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+        </ClickAwayListener>
       </div>
     </nav>
   );
 };
-
 
 export default Navbar;
