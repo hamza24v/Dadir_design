752609e0a8b52711cd479b6ebb551396cd0109e3
hamza24v <107317017+hamza24v@users.noreply.github.com>
2024-06-11 19:32:29 -0400

    added get, add, remove routes for services

--- backend/models/service.js
+++ backend/models/service.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const serviceSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true
+  },
   category: {
     type: String,
     required: true
--- backend/routes/services.js
+++ backend/routes/services.js
@@ -12,22 +12,52 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/addservice', async (req, res) => {
-  const service = new Service({
-    id: req.body.id,
-    category: req.body.category,
-    description: req.body.description,
-    newPrice: req.body.newPrice,
-    oldPrice: req.body.oldPrice,
-    include: req.body.include,
-    image: req.body.image,
-    serviceType: req.body.serviceType
-  })
+  try {
+    const services = await Service.find()
+    let id;
+    if (services.length > 0) {
+      let last_service_array = services.slice(-1)
+      let last_service = last_service_array[0]
+      id = last_service.id + 1;
+    } else {
+      id = 1
+    }
+    const service = new Service({
+      id: id,
+      category: req.body.category,
+      description: req.body.description,
+      newPrice: req.body.newPrice,
+      oldPrice: req.body.oldPrice,
+      include: req.body.include,
+      image: req.body.image,
+      serviceType: req.body.serviceType
+    })
 
-  await service.save()
-  res.json({
-    success: true,
-    description: req.body.description
-  })
+    await service.save()
+    console.log("Saved")
+    console.log(service)
+    res.json({
+      success: true,
+      description: req.body.description
+    })
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+
+})
+
+router.post('/removeservice', async (req, res) => {
+  try{
+    await Service.findOneAndDelete({ id: req.body.id })
+    console.log("Removed");
+    res.json({
+      success: true,
+      description: req.body.description
+    })
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+  
 })
 
 module.exports = router;
