9fd134e935d7160f47ea636ba28b0d1da06c6c57
hamza24v <107317017+hamza24v@users.noreply.github.com>
2024-06-12 16:57:19 -0400

    optimized gallery preview in home page

--- Ui/src/pages/Galleries.jsx
+++ Ui/src/pages/Galleries.jsx
@@ -3,23 +3,36 @@ import { galleries_text } from '../constants/galleries';
 import GalleryItem from './GalleryItem';
 import { galleryData } from '../constants/galleries';
 import Button from '@mui/material/Button';
+import { NavLink } from 'react-router-dom';
 
-function Galleries() {
+function Galleries({ showPreview }) {
+  const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+};
   return (
     <div className="flex flex-col justify-center items-center mt-20 px-4 py-8">
       <h1 className="text-3xl font-bold text-center mb-4">Galleries</h1>
-      <p className="mb-5 w-full md:w-2/3 text-center text-lg leading-relaxed text-gray-700">
+      {!showPreview && <p className="mb-5 w-full md:w-2/3 text-center text-lg leading-relaxed text-gray-700">
         {galleries_text}
-      </p>
+      </p>}
 
       <div className="flex mb-10 w-full justify-center mt-10">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4  items-center">
-          {galleryData.map((item, index) => (
-            <GalleryItem key={index} item={item} />
-          ))}
+          {galleryData.map((item, index) => {
+            if (index < 6) return <GalleryItem key={index} item={item} />
+          })}
         </div>
       </div>
-      <Button size='large' variant='contained' color='salmon'> View more</Button>
+      {!showPreview ?
+        <Button size='large' variant='contained' color='salmon'> View more</Button>
+        :
+        <NavLink to='/Galleries' onClick={scrollToTop}>
+          <Button size='large' variant='contained' color='salmon'> View more</Button>
+        </NavLink>
+      }
     </div>
   );
 }
